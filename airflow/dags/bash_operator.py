e876fb8f9a52f15840f61a53df45bf8c2e7f837b
Akshay Kumar <akshaykumar25051997@gmail.com>
2024-02-23 17:27:28 +0530

    Update bash_operator.py

--- airflow/dags/bash_operator.py
+++ airflow/dags/bash_operator.py
@@ -10,28 +10,17 @@ from airflow.operators.empty import EmptyOperator
 
 with DAG(
     dag_id="example_bash_operator",
-    schedule="0 0 * * *",
+    schedule="*/5 * * * *",
     start_date=pendulum.datetime(2024, 1, 1, tz="UTC"),
     catchup=False,
     dagrun_timeout=datetime.timedelta(minutes=60),
-    tags=["example", "example2"],
+    tags=["example", "bash"],
     params={"example_key": "example_value"},
 ) as dag:
-
-    run_this_last = EmptyOperator(
-        task_id="run_this_last",
-    )
-
+    
     # [START]
     run_this = BashOperator(
         task_id="task_id",
         bash_command="echo 'my bash command'",
     )
     # [END]
-
-    run_this >> run_this_last
-
-    
-
-if __name__ == "__main__":
-    dag.test()
